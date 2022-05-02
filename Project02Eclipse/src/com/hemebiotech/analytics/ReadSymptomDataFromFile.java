edd6f155c2b58b7bef7f7b592fdf779402eee134
Lyes Fourcroy <fourcroy.l@outlook.fr>
2022-05-02 08:30:38 +0200

    Commentaires soutenance

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -12,7 +12,7 @@ import java.util.TreeMap;
  * Simple brute force implementation
  *
  */
-public class ReadSymptomDataFromFile implements ISymptomReader { // 
+public class ReadSymptomDataFromFile implements ISymptomReader { 
 
 	private String filepath;
 	
@@ -20,30 +20,31 @@ public class ReadSymptomDataFromFile implements ISymptomReader { //
 	 * 
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
 	 */
-	public ReadSymptomDataFromFile (String filepath) { // builder contruit une instance dans la methode
+	public ReadSymptomDataFromFile (String filepath) { // builder contruit une instance de la classe ReadSymptomDataFromFile
 		this.filepath = filepath;
 	}
 	
 	@Override // surcharge de la classe dans l'interface qui a été implémentée  
 	 public  Map<String,Symptom> getSymptoms() {
-		 Map<String,Symptom> result =  new TreeMap<>(); //ranger par order alphebetique
+		 Map<String,Symptom> result =  new TreeMap<>(); //ranger par order alphebetique 
 
 			
 		if (filepath != null) {
 			try {
 				
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
+				BufferedReader reader = new BufferedReader (new FileReader(filepath));// instancie la classe Buffereader qui nous permet le lire fichier symptom.txt
 				
-				String symptom = reader.readLine();
+				String symptom = reader.readLine(); // on lit la premiere ligne.
 				
 				
-				while (symptom!= null) {
+				while (symptom!= null) { // tant que que la variable n'est pas Null
+					// lors de la lecture du fichier symptom si le symptome est connu on l'incremente de 1
 					if(result.containsKey(symptom)){
 						result.get(symptom).incrementOccurence();
 					}else{
-						result.put(symptom, new Symptom(symptom,1));
+						result.put(symptom, new Symptom(symptom,1)); // on insere le nouveau symptom  
 					}
-					symptom = reader.readLine();
+					symptom = reader.readLine(); // on lit la ligne suivante 
 				}
 				reader.close();
 			} catch (IOException e) {
@@ -51,7 +52,7 @@ public class ReadSymptomDataFromFile implements ISymptomReader { //
 			}
 		}
 		
-		return result;
+		return result; // retourne le result
 	}
 
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/SymptomWriterInterface.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomWriterInterface.java
@@ -2,8 +2,9 @@ package com.hemebiotech.analytics;
 
 import java.util.Map;
 
-public interface SymptomWriterInterface {
+public interface SymptomWriterInterface { // on déclare l'interface SWI 
 	
-	public void writeSymptom( Map<String,Symptom> result);
+	public void writeSymptom( Map<String,Symptom> result); 
+	// prototupe de la  méthode writesymptom qui nous permet d'écrire le résultat de la méthode getsymptom() dans le fichier result.out
 
 }
