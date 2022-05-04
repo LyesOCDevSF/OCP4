4801b9b3932097369a1db9037520db17f6353520
Lyes Fourcroy <fourcroy.l@outlook.fr>
2022-05-04 20:52:58 +0200

    Ajout de la javadoc Symptom

--- Project02Eclipse/src/com/hemebiotech/analytics/Symptom.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Symptom.java
@@ -1,5 +1,9 @@
 package com.hemebiotech.analytics;
 
+/**
+ * @author 33698
+ *
+ */
 public class Symptom { // cette classe représente un symptome 
    protected String nom; // accessible a cette classe et les classe filles 
    protected int occurence;
@@ -7,19 +11,31 @@ public class Symptom { // cette classe représente un symptome
 	   this.nom= nom;
 	   this.occurence= occurence;
    }
-   public String getNom() { // renvoie le nom du symptome
+   /**
+ * @return
+ */
+public String getNom() { // renvoie le nom du symptome
 	   return this.nom;   
    }
-   public int getOccurence() {
+   /**
+ * @return
+ */
+public int getOccurence() {
 	   return this.occurence;
    }
-   public void setNom(String nom) { // change le nom si nouveau nom
+   /**
+ * @param nom
+ */
+public void setNom(String nom) { // change le nom si nouveau nom
 	   this.nom = nom;  
    }
    public void setOccurence(int occurence) { // on change l'occurence
 	   this.occurence = occurence;  
    }
-   public void incrementOccurence() { // on incremente l'occurence de 1 a chaque fois que le même symptom est compté
+   /**
+ * 
+ */
+public void incrementOccurence() { // on incremente l'occurence de 1 a chaque fois que le même symptom est compté
 	 //  this.occurence = this.occurence+1;
 	   //this.occurence+=1;
 	   this.occurence++;
