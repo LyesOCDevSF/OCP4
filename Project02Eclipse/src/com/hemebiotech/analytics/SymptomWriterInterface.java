73ce698e023074cbb83ce8a60d1c148c79bd3e13
Lyes Fourcroy <fourcroy.l@outlook.fr>
2022-05-04 20:56:34 +0200

    Ajout de la javadoc Symptomwriterinterface

--- Project02Eclipse/src/com/hemebiotech/analytics/SymptomWriterInterface.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomWriterInterface.java
@@ -2,8 +2,17 @@ package com.hemebiotech.analytics;
 
 import java.util.Map;
 
+/**
+ * @author 33698
+ *
+ */
+
+ 
 public interface SymptomWriterInterface { // on déclare l'interface SWI 
 	
+	/**
+	 * @param result
+	 */
 	public void writeSymptom( Map<String,Symptom> result); 
 	// prototupe de la  méthode writesymptom qui nous permet d'écrire le résultat de la méthode getsymptom() dans le fichier result.out
 
